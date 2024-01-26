d9286db40b30e893642c1fe75833c64b24b7b7fe
Michael Maluf <malufmike@gmail.com>
2024-01-25 21:13:03 -0800

    Adds PORT and JWT_SECRET to envConfig

--- src/config/custom-env-variables.config.ts
+++ src/config/custom-env-variables.config.ts
@@ -4,7 +4,8 @@ dotenv.config();
 
 const envConfig = {
   POSTGRES_CONNECTION_STRING: process.env.POSTGRES_CONNECTION_STRING,
-  PORT: process.env.PORT || 8000
-}
+  PORT: process.env.PORT || 8000,
+  JWT_SECRET: process.env.JWT_SECRET,
+};
 
 export default envConfig;
